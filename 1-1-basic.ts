861ee53737a8ae1be193a865f02212202e8a679d
rnfkaus8 <kth4350@gmail.com>
2022-08-08 20:22:09 +0900

    number, string, boolean, undefined, null 선언

---
+++ 1-1-basic.ts
@@ -0,0 +1,29 @@
+{
+    /**
+     * JavaScript
+     * Primitive: number, string, boolean, bigint, symbol, null, undefined
+     * Object: function, array....
+     */
+        // number
+    const num: number = 10;
+
+    // string
+    const str: string = 'hello';
+
+    // boolean
+    const bool: boolean = false;
+
+    // undefined
+    let age: number | undefined;
+    age = undefined;
+    age = 1;
+    const find = (): number | undefined => {
+        return undefined;
+    };
+
+    // null
+    let person: string | null;
+    person = null;
+    person = 'kim tae hyun';
+
+}
