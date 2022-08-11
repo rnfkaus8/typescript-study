21eb4fecd16f091ab1978454d62fef58a40dc3aa
rnfkaus8 <kth4350@gmail.com>
2022-08-12 06:52:24 +0900

    Union Type  - type 에 지정된 데이터만 선언 가능

---
+++ 1-5-union.ts
@@ -0,0 +1,33 @@
+{
+    /**
+     * Union Types: OR
+     */
+    type Direction = 'left' | 'right' | 'up' | 'down';
+
+    function move(direction: Direction) {
+        console.log(direction);
+    }
+    move('right');
+
+    type TileSize = 8 | 16 | 32;
+    const tile: TileSize = 16;
+
+    type SuccessState = {
+        response: {
+            body: string;
+        }
+    };
+    type FailState = {
+        reason: string;
+    };
+    type LoginState = SuccessState | FailState
+
+    function login(id: string, password: string): LoginState {
+        return {
+            response: {
+                body: 'logged in!',
+            }
+        }
+    }
+
+}
