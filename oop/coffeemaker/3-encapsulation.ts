4fa646ce592862ad20e982da6f72e56edec7fb12
rnfkaus8 <kth4350@gmail.com>
2022-08-22 07:07:01 +0900

    생성자의 파라미터에 접근 제어자 사용 시 필드를 굳이 작성하지 않아도 됨 get : getter set : setter

--- oop/coffeemaker/3-encapsulation.ts
+++ oop/coffeemaker/3-encapsulation.ts
@@ -37,4 +37,29 @@
 
   const maker = CoffeeMaker.makeMachine(32);
   maker.fillCoffeeBeans(100);
+
+  class User {
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    private internalAge = 4;
+    get age(): number {
+      return this.internalAge;
+    }
+
+    set age(num: number) {
+      if (num < 0) {
+        throw new Error("age must be greater than 0");
+      }
+      this.internalAge = num;
+    }
+    constructor(private firstName: string, private lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  }
+  const user = new User("Steve", "Jobs");
+  user.age = 6;
+  console.log(user.fullName);
+  console.log(user.age);
 }
