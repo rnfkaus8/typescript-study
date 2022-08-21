04608643f120cd7412378cc7e0e73e124f80045f
rnfkaus8 <kth4350@gmail.com>
2022-08-21 11:41:44 +0900

    class 를 이용하여 기능 구현

---
+++ oop/coffeemaker/2-class.ts
@@ -0,0 +1,28 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAM_PER_SHOT: number = 7;
+    coffeeBeans: number = 0;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+  const coffeeMaker = new CoffeeMaker(32);
+  console.log(coffeeMaker);
+}
